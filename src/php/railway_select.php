7ad09919d92279ec1d763797d561a0539c06c8ff
4423 <hanamiya4423@gmail.com>
2014-11-14 22:02:32 +0900

    fix railway_select.
    タブを使用した際に表示が崩れる問題を修正　

--- src/php/railway_select.php
+++ src/php/railway_select.php
@@ -2,30 +2,29 @@
 <html >
 <head>
 <meta charset="UTF-8">
+<title></title>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../libs/liquidslider-master/css/liquid-slider.css"/>
-    <link rel="stylesheet" href="../css/railway_select.css">
-
+    
     <!-- Optional theme -->
     <!-- Latest compiled and minified JavaScript -->
-    <link rel="stylesheet" href="../../libs/jquery-accordion-image-menu-master/accordionImageMenu.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="../../libs//jquery/jquery-2.1.1.min.js" type="text/javascript"></script>
-	<script src="../../libs/liquidslider-master/src/js/jquery.liquid-slider.js"></script>
-  	<script src="../../libs/jquery-touchSwipe/jquery.touchSwipe.min.js"></script>
+    <link rel="stylesheet" href="../../libs/jquery-accordion-image-menu-master/accordionImageMenu.css" />
+    <script src="../../libs/jquery/jquery-2.1.1.min.js" type="text/javascript"></script>
+    <script src="../../libs/liquidslider-master/src/js/jquery.liquid-slider.js"></script>
+    <script src="../../libs/jquery-touchSwipe/jquery.touchSwipe.min.js"></script>
     <script src="../../libs/Wookmark/jquery.wookmark.js" type="text/javascript"></script>
     <script src="../../libs/ImgWheel-master/jquery.ImgWheel.min.js"></script>
     <script src="../../libs/jquery_animate_colors/jquery.animate-colors.js"></script>
-   	<script src="../../libs/btn_plugin/btn_plugin.js"></script>
-   	<script src="../../libs/jquery-accordion-image-menu-master/jquery.accordionImageMenu.min.js"></script>
-  
+    <script src="../../libs/btn_plugin/btn_plugin.js"></script>
+    <script src="../../libs/jquery-accordion-image-menu-master/jquery.accordionImageMenu.min.js"></script>
 
+    <link rel="stylesheet" href="../css/railway_select.css" />
 </head>
 <body>
     
 	<script type="text/javascript">
-		var railway_name = 'odpt.Railway:TokyoMetro.Hibiya';
+		//var railway_name = 'odpt.Railway:TokyoMetro.Hibiya';
 
 		if (railway_name == 'odpt.Railway:TokyoMetro.Ginza') {
 			var d = document;
@@ -113,76 +112,104 @@
 	</script>
 
 	<?php		
-        // $start_station = htmlspecialchars($_GET['station']);
-       	// $railway_name = "";
-    ?>    
+                //$start_station = htmlspecialchars($_GET['station']);
+                $start_station = "上野";       	
+        ?>    
 
 	<?php
-		require 'metro.php';
-		$metro = new metro();
-		// $start_station_info = $metro->station($start_station);
-        // $start_code = reset($start_station_info)->{"odpt:stationCode"};
-        // $start_code = "T10"; //trainページのテスト用
-        // $connecting_railway = $metro->connectingMetroRailway(reset($start_station_info));
-        // var_dump($connecting_railway);
-        //ここに路線名(日本語)から路線名(sameAs)に変換する何かを記述
-		$dc_title = "上野";
-		$railway_name = $metro->station($dc_title)[0]->{"odpt:railway"};
-		$connecting_railway_name = $metro->station($dc_title)[0]->{"odpt:connectingRailway"};
-		$array = array($railway_name);
-		$array = array_merge($array, $connecting_railway_name);
-		foreach ($array as $key => $value) {
-			if ($value == null)
-				unset($array[$key]);
-		}
+		require 'metro.php';		
+                $metro = new metro();
+                
+                //駅名に一致する駅情報のうち先頭の要素を取得し、駅コードと全ての接続路線を取得
+		$start_station_info = reset($metro->station($start_station));
+                $start_code = $start_station_info->{"odpt:stationCode"};
+                $connecting_railway = $metro->connectingMetroRailway($start_station_info);
 	?>
 
 	<!-- <div id="header">行き先を決めてください</div> -->
+        <!--
 	<?php
-		echo '<div class="liquid-slider" id="slider">';
-		// 駅の数だけタブ生成
-		foreach ($array as $key => $value) {
-					echo '<div id="contents">';
-					echo '<h2 class="title" >'.$value.'</h2>';
-					echo '<div id="tiles" style="position: relative">';
-					echo '<ul　id="list">';
-					$stations = $metro->stations($metro->railway_eng($value));
-					// タブごとにに駅を表示
-					foreach ($stations as $key=>$value) {
-						$end_code = $value["odpt:stationcode"];
-						echo '<li id="li">';
-                        echo '<form action="../../src_train/php/train.php" method="post">';
-					 	echo '<input type="hidden" name="start" value="'.$start_code.'">';
-					 	echo '<input type="hidden" name="end" value="'.$end_code.'">';
-                        echo '<input type="hidden" name="railway" value='.$railway.'>';
-						echo '<button name="send_button" type="submit" class="btn btn-1 btn-1d">';
-     					echo '<span class="text">';
-     					echo '<div class="station_jp_name">',$value["station_jp_name"],'</div>';
-     					echo '<div class="station_eng_name">',$value["station_eng_name"],'</div>';
-     					echo '</span>';
-     					echo '<span class="back">';
-         				echo '<span class="left"></span>';
-         				echo '<span class="right"></span>';
-     					echo '</span>';
-						echo '</button>';
-                        echo '</form>';
-						echo '</li>';	
-					}	
-
-					echo '</ul>';
-					echo '</div>';
-					echo '</div>';
-
-				}
-			
-	echo '</div>';
-	?>	
+                //**********  下のHTMLをPHPメインに書くとこんな感じなver  ************
+//               
+//                
+//		echo '<div class="liquid-slider" id="slider">';
+//		// 駅の数だけタブ生成
+//		foreach ($connecting_railway as $index => $railway) {
+//                        echo '<div id="contents">';
+//                        echo '<h2 class="title" >'.$railway.'</h2>';
+//                        echo '<div id="tiles">';
+//                        echo '<ul>';
+//                        $stations = $metro->stations($metro->railway_eng($railway));
+//                        // タブごとにに駅を表示
+//                        foreach ($stations as $key => $value) {
+//                                $end_code = $value["odpt:stationcode"];
+//                                echo '<li>';
+//                                echo '<form action="../../src_train/php/train.php" method="post">';
+//                                echo '<input type="hidden" name="start" value="'.$start_code.'">';
+//                                echo '<input type="hidden" name="end" value="'.$end_code.'">';
+//                                echo '<button name="send_button" type="submit" class="btn btn-1 btn-1d">';
+//                                echo '<span class="text">';
+//                                echo '<div class="station_jp_name">',$value["station_jp_name"],'</div>';
+//                                echo '<div class="station_eng_name">',$value["station_eng_name"],'</div>';
+//                                echo '</span>';
+//                                echo '<span class="back">';
+//                                echo '<span class="left"></span>';
+//                                echo '<span class="right"></span>';
+//                                echo '</span>';
+//                                echo '</button>';
+//                                echo '</form>';
+//                                echo '</li>';	
+//                        }
+//                        echo '</ul>';
+//                        echo '</div>';
+//                        echo '</div>';
+//                }			
+//                echo '</div>';
+	?>
+        -->
+        
+        <div class="liquid-slider" id="slider">
+            <?php // 駅の数だけタブ生成
+            foreach ($connecting_railway as $index => $railway) {
+            ?>
+                <div id="contents">
+                    <h2 class="title" ><?php echo $railway ?></h2>
+                    <div id="tiles">
+                        <ul>
+                        <?php // タブごとにに駅を表示
+                        $stations = $metro->stations($metro->railway_eng($railway));                        
+                        foreach ($stations as $key => $value) {
+                            $end_code = $value["odpt:stationcode"];
+                        ?>
+                            <li>
+                                <form action="../../src_train/php/train.php" method="post">
+                                    <input type="hidden" name="start" value="<?php echo $start_code ?>">
+                                    <input type="hidden" name="end" value="<?php echo $end_code ?>">
+                                    <button name="send_button" type="submit" class="btn btn-1 btn-1d">
+                                        <span class="text">
+                                            <div class="station_jp_name"><?php echo $value["station_jp_name"] ?></div>
+                                            <div class="station_eng_name"><?php echo $value["station_eng_name"] ?></div>
+                                        </span>
+                                        <span class="back">
+                                            <span class="left"></span>
+                                            <span class="right"></span>
+                                        </span>
+                                    </button>
+                                </form>
+                            </li>
+                        <?php } ?>
+                        </ul>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
 
 
 	<script>
-		$(function() {
-			$('#slider').liquidSlider({
-				autoHeight: true,
+		setTimeout("slider()", 5);                
+                function slider(){
+                    $('#slider').liquidSlider({
+                        autoHeight: true,
     			slideEaseFunction: "easeInOutExpo",
     			autoSlide: false,
     			dynamicArrows: true,
@@ -196,24 +223,82 @@
     			responsive: true,
    			 	mobileNavigation: true,
     			mobileNavDefaultText: 'Menu',
-    		});
-		});
- 
+                    });
+                } 
 	</script>
 
+        
 	<script type="text/javascript">
   		$(document).ready(new function() {
-    		var options = {
+                    //woolmark();
+                 });
+                 
+                function wookmark(){
+                    var options = {
       			autoResize: true, // ブラウザの拡大縮小に合わせて要素を自動でリサイズするかどうか
       			container: $('#contents'), // CSSを適用している要素を指定
       			offset: 40, // 要素間の隙間を指定
       			itemWidth: 600,
       			itemHeight:300 // 各要素の幅を指定 
-    		};
-    	var handler = $('#tiles li'); // レンガ状にする要素を指定
-       
-    	handler.wookmark(options); // wookmarkをオプション付きで実行
-		});
+                    };
+                    var handler = $('#tiles li'); // レンガ状にする要素を指定
+                    handler.wookmark(options); // wookmarkをオプション付きで実行
+                }
 	</script>
+        
+        <script type="text/javascript">
+            //#slider-nav-ul のロード後にchangeColor()を呼び出したいが、$()で上手くいかないので適当に待つ
+            setTimeout("changeColor()", 5);
+            
+            var themeColor = {
+                "銀座線":"#FF9500",
+                "丸ノ内線":"#F62E36",
+                "日比谷線":"#B5B5AC",
+                "東西線" :"#009BBF",
+                "千代田線":"#00BB85",
+                "有楽町線":"#C1A470",
+                "半蔵門線":"#8F76D6",
+                "南北線":"#00AC9B",
+                "副都心線":"#9C5E31"
+            };
+            
+            function changeColor(){
+                //タブ選択部分の各li要素に対する処理
+                $('#slider-nav-ul li').each(function(){
+                    var railway = $(this).text();                    
+                    for (var key in themeColor){
+                        if (railway === key) {
+                            //背景色の変更
+                            $(this).children("a").css({"background-color": themeColor[key]});
+                        }
+                    }                    
+                });
+                
+                //div#contents 配下の色変更
+                $('div #contents').each(function(){
+                    console.log(this);
+                    var railway = $(this).find("h2").text(); 
+                    for (var key in themeColor){
+                        if (railway === key) {
+                            $(this).css({"background-color": themeColor[key]});
+                            $(this).find(".btn-1").css({"background": themeColor[key]});
+                            $(this).find(".btn-1d .back span").css({"background": themeColor[key]});
+                        }
+                    }                    
+                });
+                
+                //hoverは上で指定することが出来ない
+                $(".btn-1").hover(
+                    function(){
+                        //既にbtnに設定されている色を割り当てる
+                        var color = $(this).css("background-color");
+                        $(this).css("color", color);
+                    }, 
+                    function(){
+                        $(this).css("color", "#fff");
+                    }
+                );
+            }
+        </script>
 </body>
 </html>
